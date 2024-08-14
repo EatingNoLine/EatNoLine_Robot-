d7fdb207e28b9c7692e617beee077c0113d13681
AzarAI <azarai@mail.ustc.edu.cn>
2024-08-15 05:55:43 +0800

    make pump's state related to pump itself instead of it's controller

--- User/Pump/pump.c
+++ User/Pump/pump.c
@@ -10,15 +10,15 @@ void Pump_Init(Pump_TypeDef *pmp, GPIO_TypeDef *port, uint32_t pin){
 
   // Set up state of pump
   pmp->state = PUMP_STATE_OFF;
-  HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
 }
 void Pump_On(Pump_TypeDef *pmp){
   pmp->state = PUMP_STATE_ON;
-  HAL_GPIO_WritePin(pmp->port, pmp->pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(pmp->port, pmp->pin, GPIO_PIN_RESET);
 }
 void Pump_Off(Pump_TypeDef *pmp){
   pmp->state = PUMP_STATE_OFF;
-  HAL_GPIO_WritePin(pmp->port, pmp->pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(pmp->port, pmp->pin, GPIO_PIN_SET);
 }
 void Pump_Toggle(Pump_TypeDef *pmp){
   if (pmp->state == PUMP_STATE_ON) {
